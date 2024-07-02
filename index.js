9934a9edcd8adaffbd1e670527f479e498567b09
邓仕昊[sx_dancehole] <sx_dancehole@canway.net>
2024-07-02 18:21:31 +0800

    feature: 1.0

--- index.js
+++ index.js
@@ -1,28 +1,242 @@
 // index.html 对应js逻辑
 
-
 // 程序基本逻辑：类似于 共享位置信息，区别在于轮询信息 + 动画展示
+var MAP // 全局变量：地图对象
+var NAME // 用户名
+var is_Animating = false
 
-var MAP
-async function getLocation() {
-  if (await navigator.geolocation) {
-    return navigator.geolocation.getCurrentPosition(showPosition, showError);
+// 显示自定义模态框
+function showPrompt() {
+  const n = getCookie('name')
+  console.log(n)
+  if (n !== null && n !== '') {
+    NAME = n
+    document.getElementById('customPrompt').style.display = 'none';
+    getLocation()
+  } else {
+    document.getElementById('customPrompt').style.display = 'block';
+  }
+}
+
+function getCookie(name) {
+  var cookieArr = document.cookie.split('; '); // 将cookie字符串分割成多个条目
+  for (var i = 0; i < cookieArr.length; i++) {
+    var cookiePair = cookieArr[i].split('='); // 分割键值对
+    if (name == cookiePair[0]) { // 如果找到了匹配的键
+      // 解码Cookie值并返回
+      return decodeURIComponent(cookiePair[1]);
+    }
+  }
+  // 如果没有找到匹配的键，返回null
+  return null;
+}
+
+function setCookie(name, value, daysToExpire) {
+  /**
+   * 设置Cookie
+   * 
+   * @param {string} name - Cookie的名称
+   * @param {string} value - Cookie的值
+   * @param {number} daysToExpire - Cookie过期的天数，如果不设置则默认为会话Cookie（关闭浏览器即失效）
+   */
+  var expires = "";
+  if (daysToExpire) {
+    var date = new Date();
+    date.setTime(date.getTime() + (daysToExpire * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// 处理用户点击确定按钮的逻辑【程序入口文件】
+function handleConfirm() {
+  const name = document.getElementById('promptInput').value;
+  setCookie('name', name, 1); // 把名字放到cookie里，这样刷新页面的时候，还能拿到用户输入的内容/不用每次都输入名字
+  console.log('用户输入的内容：', name)
+  document.getElementById('customPrompt').style.display = 'none';
+  // 更新地图
+  getLocation();
+}
+
+// 获取位置权限，并通过showPosition和showError回调展示（本身不返回值）
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
   } else {
     console.error('当前浏览器不支持地理定位')
   }
 }
+
+// 上传自己的信息，同时下拉别人的信息(info:{name:,latitude:,longitude:})
+async function getAndPostPosition(info) {
+  try {
+    // 提交fetch请求=>上传位置信息=>下拉所有人的位置信息
+    let response = await fetch("http://119.91.149.12:7705/submit_location", {
+      method: "POST",
+      body: JSON.stringify(info),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    if (response.ok) {
+      console.log("提交成功！", response);
+      return response.json();
+    } else {
+      console.error("提交失败，请重试！", response);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  return null
+}
+
+function calculateMid(hasLoaded) {
+  let sumLatitudes = 0;
+  let sumLongitudes = 0;
+  let count = hasLoaded.length;
+
+  for (let location of hasLoaded) {
+    console.log(location)
+    sumLatitudes += location.latitude;
+    sumLongitudes += location.longitude;
+  }
+
+  console.log(sumLatitudes / count, sumLongitudes / count);
+  const mid = {
+    latitude: sumLatitudes / count,
+    longitude: sumLongitudes / count
+  }
+
+  // 打标
+  document.getElementById('text').innerHTML = '正在计算中心！'
+  const tmpLocate = new qq.maps.LatLng(mid.latitude, mid.longitude);
+  MAP.panTo(tmpLocate);
+  MAP.zoomTo(12);
+
+  const marker = new qq.maps.Marker({
+    position: tmpLocate,
+    animation: qq.maps.MarkerAnimation.DROP,
+    map: MAP
+  });
+
+  // 进行打标和文本提示
+  setTimeout(() => {
+
+
+    setTimeout(() => {
+      var label = new qq.maps.Label({
+        position: tmpLocate,
+        map: MAP,
+        content: res.name
+      });
+
+      is_Animating = false  // set false
+      document.getElementById('text').innerHTML = ''
+    }, 1000);
+    MAP.zoomTo(16);
+  }, 2500);
+
+
+  return {
+    averageLatitude: sumLatitudes / count,
+    averageLongitude: sumLongitudes / count
+  };
+}
+
+async function render(res) {
+  is_Animating = true
+  document.getElementById('text').innerHTML = '检测到' + res.name + '的位置！'
+  // 缩小画面，展示其他人的位置信息【计算中心位置】
+
+  // 渲染
+  const tmpLocate = new qq.maps.LatLng(res.latitude, res.longitude);
+  MAP.panTo(tmpLocate);
+  MAP.zoomTo(12);
+
+  const marker = new qq.maps.Marker({
+    position: tmpLocate,
+    animation: qq.maps.MarkerAnimation.DROP,
+    map: MAP
+  });
+
+  // 进行打标和文本提示
+  setTimeout(() => {
+
+    MAP.zoomTo(16);
+
+    setTimeout(() => {
+      var label = new qq.maps.Label({
+        position: tmpLocate,
+        map: MAP,
+        content: res.name
+      });
+
+      is_Animating = false  // set false
+      document.getElementById('text').innerHTML = ''
+    }, 1000);
+  }, 2500);
+}
+// 主函数
+async function loop(curr) {
+
+  const initialInterval = 7000; // 初始时间间隔，单位毫秒
+  const params = {
+    name: document.getElementById('promptInput').value,
+    latitude: curr.latitude,
+    longitude: curr.longitude
+  }
+
+  // 轮询=>渲染
+  const hasLoaded = [params.name] // 已经渲染过的名字
+  const hasLoadedAll = [params]
+  let is_calculated = false;
+
+  const timer = setInterval(() => {
+    const res = getAndPostPosition(params);
+    res.then(data => {
+      if (data.length > 0) {
+        data.forEach(item => {
+          if (!hasLoaded.includes(item.name) && !is_Animating) {
+            hasLoaded.push(item.name);
+            hasLoadedAll.push(item)
+            render(item);
+          }
+        })
+      }
+    })
+
+    if (hasLoaded.length === 4 && !is_calculated) {
+      is_calculated = true
+      calculateMid(hasLoadedAll)
+    }
+
+  }, initialInterval);
+}
+
 // 获取定位成功，显示位置信息
 async function showPosition(position) {
   var curr = position.coords
+
+  // 发请求,不求结果
+  const params = {
+    name: document.getElementById('promptInput').value,
+    latitude: curr.latitude,
+    longitude: curr.longitude
+  }
+  getAndPostPosition(params);
+
   console.log(
     'Latitude(纬度): ' + position.coords.latitude + // 纬度
     '<br>Longitude(经度): ' + position.coords.longitude // 经度
   )
+
+  // 给自己设置
   MAP.panTo(new qq.maps.LatLng(position.coords.latitude, position.coords.longitude));
   MAP.zoomTo(16);
   // 打标
   setTimeout(function () {
-    const marker = new qq.maps.Marker({
+    new qq.maps.Marker({
       position: new qq.maps.LatLng(position.coords.latitude, position.coords.longitude),
       animation: qq.maps.MarkerAnimation.DROP,
       map: MAP
@@ -30,105 +244,15 @@ async function showPosition(position) {
     //marker.setAnimation(qq.maps.Animation.DROP);
   }, 1000);
 
-  // 弹出输入框
-  const name = await prompt("请输入您的名字:");
-  console.log(name)
-
   // 文本提示
-  var label = new qq.maps.Label({
+  new qq.maps.Label({
     position: new qq.maps.LatLng(position.coords.latitude, position.coords.longitude),
     map: MAP,
-    content: name
+    content: params.name
   });
 
-  try {
-    // 提交fetch请求=>上传位置信息=>下拉所有人的位置信息
-    let response = await fetch("https://example.com/submit", {
-      method: "GET",
-      body: JSON.stringify({ name }),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    });
-
-    if (response.ok) {
-      alert("提交成功！");
-    } else {
-      alert("提交失败，请重试！");
-    }
-  } catch (error) {
-    console.log(error);
-  }
-
-  // 缩小画面，展示其他人的位置信息【计算中心位置】
-  response = [{
-    "name": "aaa",
-    "latitude": 23.135259591059862,
-    "longitude": 113.32640785925591
-  },
-  {
-    "name": "bbb",
-    "latitude": 23.135852591059862,
-    "longitude": 113.32312785925591
-  },
-  {
-    "name": "ccc",
-    "latitude": 23.135852591059862,
-    "longitude": 113.31215785925591
-  },
-  ]
-
-  // 循环渲染
-  response.forEach((element, index) => {
-    setTimeout(() => {
-      const tmpLocate = new qq.maps.LatLng(element.latitude, element.longitude);
-      MAP.panTo(tmpLocate);
-      MAP.zoomTo(16);
-
-      // 进行打标和文本提示
-      setTimeout(() => {
-        const marker = new qq.maps.Marker({
-          position: tmpLocate,
-          animation: qq.maps.MarkerAnimation.DROP,
-          map: MAP
-        });
-
-        var label = new qq.maps.Label({
-          position: tmpLocate,
-          map: MAP,
-          content: element.name
-        });
-      }, 2000);
-    }, index * 4000); // 根据索引乘以延迟时间，以实现逐个渲染位置信息的效果
-    console.log('move to:', element)
-  });
-
-  // 打完标后，计算中心距离
-  // 计算中心位置
-  const centerPos = {
-    lat: response.reduce((sumLat, item) => sumLat + item.latitude, 0) / response.length,
-    lng: response.reduce((sumLng, item) => sumLng + item.longitude, 0) / response.length
-  };
-
-  console.log('Center Position:', centerPos);
-
-  // 平移到中心位置
-  const centerLatLng = new qq.maps.LatLng(centerPos.lat, centerPos.lng);
-  MAP.panTo(centerLatLng);
-  MAP.zoomTo(16);
-
-  const marker = new qq.maps.Marker({
-    position: centerLatLng,
-    animation: qq.maps.MarkerAnimation.DROP,
-    map: MAP
-  });
-
-  var label = new qq.maps.Label({
-    position: centerLatLng,
-    map: MAP,
-    content: 'Center Position'
-  });
-
+  // 跳转去loop 监听
+  loop(curr)
   return curr
 }
 // 获取定位失败，显示错误信息
@@ -149,16 +273,15 @@ var init = function (curr) {
   });
 }
 
-window.onload = async function () {
+// 初始化地图
+window.onload = function () {
+
+  // 检查是否需要输入名字
+  showPrompt()
   try {
     // 初始化地图为gz
     init({ latitude: 23.13586376420126, longitude: 113.3245201066893 });
-
-    // 更新地图
-    await getLocation();
-
   } catch (error) {
     console.error("获取位置失败:", error);
   }
-
 }
---
+++ server.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# 模拟存储所有用户位置信息的列表
+users_positions = []
+    # {
+    #     "latitude": 23.145857729967963,
+    #     "longitude": 113.32596346177941,
+    #     "name": "111"
+    # },
+    # {
+    #     "latitude": 23.15585688254556,
+    #     "longitude": 113.32608734029658,
+    #     "name": "222"
+    # },
+    # {
+    #     "latitude": 23.165855713764762,
+    #     "longitude": 113.32602920341634,
+    #     "name": "333"
+    # }
+
+
+@app.route('/submit_location', methods=['POST'])
+def submit_and_get_positions():  
+    # 获取请求中的JSON数据
+    user_info = request.get_json()
+    # print('recv:',user_info)
+    print(users_positions)
+    
+    if not user_info or 'name' not in user_info or 'latitude' not in user_info or 'longitude' not in user_info:
+        return jsonify({"error": "Invalid data format. Expected name, latitude, and longitude."}), 400
+    
+    # 存储用户的位置信息
+        # 检查是否有重复的名字
+    for existing_info in users_positions:
+        if existing_info.get('name') == user_info['name']:
+            return jsonify(users_positions), 200
+    
+    # 存储用户的位置信息（如果没有重复）
+    print('正在插入新数据')
+    users_positions.append(user_info)
+    
+    # 返回除了最新提交的之外的所有位置信息
+    # 注意：在实际应用中，您可能需要考虑分页、过滤或从数据库中高效查询
+    return jsonify(users_positions), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5033)
